8a926d9237f2e609c6a030445664b88049df2d50
Joerdon Fryeman <JoerdonFryeman@proton.me>
2026-02-20 17:17:40 +0100

    19. В модуле main.py изменён обработчик корректного завершения.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import signal
+from time import sleep
+
 from core.run import RunProgram
 
 run = RunProgram()
@@ -7,14 +9,14 @@ run = RunProgram()
 def main(name: str, version: str, year: int) -> None:
     """Запускающая все процессы главная функция."""
 
-    def get_handler(signum, frame) -> None:
+    def get_handler(signum, _frame) -> None:
         run.running = False
         run.logger.info('Задействован обработчик сигналов для корректного завершения: %s', signum)
 
     handler_tuple: tuple[str, str, str] = ('SIGHUP', 'SIGINT', 'SIGTERM')
-    for name in handler_tuple:
-        if hasattr(signal, name):
-            signal.signal(getattr(signal, name), get_handler)
+    for n in handler_tuple:
+        if hasattr(signal, n):
+            signal.signal(getattr(signal, n), get_handler)
 
     try:
         run.create_directories()
@@ -22,14 +24,18 @@ def main(name: str, version: str, year: int) -> None:
         run.log_app_release(name=name, version=version, year=year)
         run.start_stream()
         run.create_wrapped_threads()
+        while getattr(run, 'running', True):
+            sleep(0.1)
         run.stop_stream()
     except Exception as e:
-        run.logger.exception(f'Проверка выдала ошибку: {e}\nЕсли не был выполнен выход в терминал, нажми Enter.')
+        run.logger.error(f'Проверка выдала ошибку: {e}\nЕсли не был выполнен выход в терминал, нажми Enter.')
         try:
             run.running = False
             run.stop_stream()
         except Exception:
-            run.logger.exception('Не удалось корректно остановить аудиострим!')
+            run.logger.exception(
+                'Не удалось корректно остановить приложение!\nЕсли не был выполнен выход в терминал, нажми Enter.'
+            )
 
 
 if __name__ == '__main__':
